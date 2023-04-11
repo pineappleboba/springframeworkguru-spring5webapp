a7a60b53687337b31417fbdf314f03f4892925ef
Luana <pineappleboba@github.com>
2023-04-11 11:13:38 -0700

    finished with step 14, creating and opening IntelliJ project

--- src/test/java/guru/springframework/spring6webapp/Spring6webappApplicationTests.java
+++ src/test/java/guru/springframework/spring6webapp/Spring6webappApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5webapp;
+package guru.springframework.spring6webapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Spring5webappApplicationTests {
+class Spring6WebappApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
